53495eec1166de3eb70ec1dc434ac87940b9636d
Anderson Nunes <andersonbruno.ns@gmail.com>
2020-02-14 12:20:16 -0300

    Loop
    mod. 1 dev de games em java.

---
+++ loop.java
@@ -0,0 +1,23 @@
+public class loop {
+
+    public static void main(String[] args) {
+        
+        int contador = 1;
+        
+        contador = 0;
+        do{
+            System.out.println(contador);
+            contador++;
+        }while(contador < 3);
+            
+        }
+    }
+
+
+
+
+
+            
+        
+            
+
--- mod. 1/loop.java
+++ mod. 1/loop.java
@@ -1,26 +1,29 @@
 public class loop {
 
     public static void main(String[] args) {
-        int contador = 0;
         boolean cont = false;
 
-                //WHILE
-        if(cont == true){
+        /* 
+        //WHILE
+        if (cont == true) {
             while (contador <= 10) {
                 System.out.println("Utilizando o while, contador esta em: " + contador);
                 contador++;
             }
-            }
-                //FOR
-        else{
-            for (int i = 1; i <= 10; i++){
+        }
+        // FOR
+        else {
+            for (int i = 1; i <= 10; i++) {
                 System.out.println("Utilizando o for, Contador esta em: " + i);
             }
-        }    
-            
-        }
+        }*/
+        int contador = 0;
+        do {
+            System.out.println(contador);
+            contador++;
+        } while (contador <= 3);
     }
-
+}
 
 
 
